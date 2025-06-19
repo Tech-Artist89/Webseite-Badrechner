3cacc1f3db55a18dab501da7e490608fa7ec5439
Sascha Richter <sascha-richter@tech-artist.de>
2025-06-19 13:41:47 +0200

    enviroments angepasst

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.mitra-sanitaer.de',
+  apiUrl: 'https://api.mitra-sanitaer.de/api',
   appName: 'Mitra Sanitär GmbH',
   version: '1.0.0',
   features: {
